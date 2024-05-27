fe55599e7866fe2a03e8db227fc774a210b0090b
oneleaon <devike975@gmail.com>
2024-05-26 21:52:47 -0400

    Styles the navbar with some separator lines

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,20 +4,25 @@ export default function Root() {
   return (
     <div class='sidebar'>
       <div><Link to='/' class='sidebarh1'>Sections</Link></div>
+      <hr class='eighty'/>
       <nav>
         <ul>
           <li>
             <Link to='/' class='sidebarLink'>Home</Link>
           </li>
+          <hr class='ten' />
           <li>
             <Link to='/aboutme' class='sidebarLink'>About Me</Link>
           </li>
+          <hr class='ten' />
           <li>
             <Link to='/skills' class='sidebarLink'>Skills</Link>
           </li>
+          <hr class='ten' />
           <li>
             <Link to='/experience' class='sidebarLink'>Experience</Link>
           </li>
+          <hr class='ten' />
           <li>
             <Link to='/education' class='sidebarLink'>Education</Link>
           </li>
