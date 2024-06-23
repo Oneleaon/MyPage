df2a0a2048e9999627bfdd294d25b715443a59a3
Oneleaon <devike975@gmail.com>
2024-06-23 18:48:02 -0400

    Adds a background for the webpage and a simple background to hold the text. Fixes middle spacing issues with the nav links

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,28 +4,30 @@ import { Link } from 'react-router-dom';
 export default function() {
   return(
     <>
+    <div class='bgBody'>
       <div class='mainbody'>
         <h1>Welcome to My Page!</h1>
-        <p class='secret'>There may be secret text around</p>
         <p>Welcome to my corner of the internet!</p>
         <p>Enjoy your stay and you're welcome to learn more about me while you're here.</p>
+        <div class='row'>
+          <div class='col'>
+            <Link to='/aboutme' class='homeLink'>About me</Link>
+          </div>
+          <div class='col'>
+            <Link to='/aboutme' class='homeLink'>Skills</Link>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col'>
+            <Link to='/aboutme' class='homeLink'>Education</Link>
+          </div>
+          <div class='col'>
+            <Link to='/aboutme' class='homeLink'>Experience</Link>
+          </div>
+        </div>
+        <p class='end'>Made by: Alexis Hean</p>
       </div>
-      <div class='row'>
-        <div class='col'>
-          <Link to='/aboutme' class='homeLink'>About me</Link>
-        </div>
-        <div class='col'>
-          <Link to='/aboutme' class='homeLink'>Skills</Link>
-        </div>
-      </div>
-      <div class='row'>
-        <div class='col'>
-          <Link to='/aboutme' class='homeLink'>Education</Link>
-        </div>
-        <div class='col'>
-          <Link to='/aboutme' class='homeLink'>Experience</Link>
-        </div>
-      </div>
+    </div>
     </>
   );
 }
