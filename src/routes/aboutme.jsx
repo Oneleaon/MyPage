ecf852a6b6499e43e21cc4281ad571a9252f28ec
Oneleaon <devike975@gmail.com>
2024-06-26 18:46:49 -0400

    Start the about me page. Add the bg to the text

--- src/routes/aboutme.jsx
+++ src/routes/aboutme.jsx
@@ -4,8 +4,19 @@ export default function AboutMe() {
   return(
     <>
       <Root />
-      <div class='mainbody'>
-        <h1>About Me</h1>
+      <div class='bgBody'>
+        <div class='mainbody'>
+          <h1>About Me</h1>
+          <p>
+          Hello, I'm Alexis. I started programming at 15 learning the basics with Visual Basic followed by Java, which would be the language I used for my first big project.
+          My friend and I were able to recreate a chess board with all the pieces moving as they should. The was no ai, so it was always a two player game. Unless you have fun playing chess against yourself.
+          We also made a silly game mode where once you take a piece from your opponent, a random piece spawns onto your side in your colour.
+          </p>
+          <p>
+            
+          </p>
+          <p class='end'>Made by: Alexis Hean</p>
+        </div>
       </div>
     </>
   );
